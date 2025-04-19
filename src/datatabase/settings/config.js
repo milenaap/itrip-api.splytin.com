6c75bd31e30f032a4f8a1da4f877c0cbfe282294
dilandarren <dilandarren@gmail.com>
2025-04-19 14:48:03 +0200

    comprobando

--- src/datatabase/settings/config.js
+++ src/datatabase/settings/config.js
@@ -1,8 +1,10 @@
 // database.js
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+console.log(process.env.DB_DATABASE);
+
+const sequelize = new Sequelize('node_test','root', '123456', {
+  host: 'localhost',
   dialect: 'mysql',
 });
 
