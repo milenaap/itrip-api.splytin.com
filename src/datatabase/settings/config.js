833b4cc8cc0318a796610921e09f49854da959bd
Dorian Gonzalez <doriandarren1@gmail.com>
2025-04-19 14:58:04 +0200

    add dotenv

--- src/datatabase/settings/config.js
+++ src/datatabase/settings/config.js
@@ -1,6 +1,8 @@
 // database.js
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+
+
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
