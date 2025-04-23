c80565d7152cc98af0ddf550eaa438eaa3ff4ab1
Dorian Gonzalez <doriandarren1@gmail.com>
2025-04-23 22:11:06 +0200

    add item example

---
+++ src/routes/itemRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { itemListController } from "../controllers/items/itemListController.js";
+
+
+const router = Router();
+
+
+router.get('/', itemListController);
+
+
+
+export default router;
