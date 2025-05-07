616110a8e15e912a0e37f5a97590aa290c760c32
Dorian Gonzalez <doriandarren1@gmail.com>
2025-05-07 22:32:17 +0200

    add check

--- src/helpers/validators/checkIdExists.js
+++ src/helpers/validators/checkIdExists.js
@@ -1,9 +1,9 @@
 // src/validators/checkIdExists.js
 export const checkIdExists = (Model) => {
-    return async (id) => {
-      const record = await Model.findByPk(id);
-      if (!record) {
-        throw new Error(`${Model.name} with ID ${id} not found`);
-      }
-    };
+  return async (id) => {
+    const record = await Model.findByPk(id);
+    if (!record) {
+      throw new Error(`${Model.name} with ID ${id} not found`);
+    }
   };
+};
